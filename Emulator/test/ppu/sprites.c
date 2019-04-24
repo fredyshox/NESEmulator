53ff57912751e580be463526713c1ad8b4314f08
Kacper Raczy <gfw.kra@gmail.com>
2019-04-24 19:18:40 +0200

    Sprite pixel layout test

--- Emulator/test/ppu/sprites.c
+++ Emulator/test/ppu/sprites.c
@@ -67,7 +67,57 @@ void test_sprite_eval() {
 }
 
 void test_sprite_pixel_layout() {
+  bool res = true;
+  // these are visible on y_coord
+  // so we care only about x_coord
+  ppu_sprite sprites[8];
+  uint8_t pixelbuf[256];
+  uint8_t expected;
+  // stacked
+  sprites[0].x_coord = 8;
+  sprites[1].x_coord = 12;
+  sprites[2].x_coord = 16;
+  // overlap
+  sprites[3].x_coord = 32;
+  sprites[4].x_coord = 32;
+  // next to
+  sprites[5].x_coord = 64;
+  sprites[6].x_coord = 72;
+  // edge
+  sprites[7].x_coord = 252;
+  // pixelbuf init
 
+  ppu_sprite_pixel_layout(sprites, 8, pixelbuf, 256);
+
+  int i;
+  for (i = 0; i < 256 && res; i++) {
+    if (i >= 8 && i < 12) {
+      expected = 0x01; // 0b00000001
+    } else if (i >= 12 && i < 16) {
+      expected = 0x03; // 0b00000011
+    } else if (i >= 16 && i < 20) {
+      expected = 0x06; // 0b00000110
+    } else if (i >= 20 && i < 24) {
+      expected = 0x04; // 0b00000100
+    } else if (i >= 32 && i < 40) {
+      expected = 0x18; // 0b00011000
+    } else if (i >= 64 && i < 72) {
+      expected = 0x20; // 0b00100000
+    } else if (i >= 72 && i < 80) {
+      expected = 0x40; // 0b01000000
+    } else if (i >= 252) {
+      expected = 0x80; // 0b10000000
+    } else {
+      expected = 0x00;
+    }
+    res = (expected == pixelbuf[i]);
+  }
+  ASSERT_EQ(256, i, "(1) sprite pixel layout", &res)
+  if (!res) {
+    fprintf(stderr, "\033[0;31m(1) pixel val: %x\033[0m\n", pixelbuf[i]);
+  }
+
+  assert(res);
 }
 
 int main(int argc, char** argv) {
