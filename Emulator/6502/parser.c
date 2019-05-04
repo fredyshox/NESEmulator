fbb8b295a1cfbc9bfd27af8b483c566dfda9531c
Kacper Raczy <gfw.kra@gmail.com>
2019-05-04 18:19:05 +0200

    Added cycle count for each cpu instruction

--- Emulator/6502/parser.c
+++ Emulator/6502/parser.c
@@ -6,6 +6,7 @@
 #include "parser.h"
 
 int execute_asm(state6502 *state) {
+  // handle interrupt (change pc) here
   asm6502 operation;
   int consumed = parse_asm(&operation, state->memory, state->pc);
   state->pc += (uint16_t) consumed;
@@ -16,9 +17,9 @@ int execute_asm(state6502 *state) {
 int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
   uint8_t opcode = memory6502_load(memory, pos);
   uint16_t mpos = pos + 1;
-  int consumed = 1, cycles = 0;
+  int consumed = 1, cycles = 2; // default 2 machine cycles
   mem_addr addr;
-  // TODO cycles
+  // TODO support for page boundary additional cycles and branch taken cycles
   switch (opcode) {
     // ADC - add with carry
     case 0x69:
@@ -26,24 +27,31 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto adc;
     case 0x6d:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto adc;
     case 0x7d:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto adc;
     case 0x79:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto adc;
     case 0x65:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto adc;
     case 0x75:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto adc;
     case 0x61:
       consumed += indexed_indirect_addr(&addr, memory, mpos);
+      cycles = 6;
       goto adc;
     case 0x71:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 5;
       adc:
         cmd->type = ADC_ASM;
         cmd->handler = add_with_carry;
@@ -54,24 +62,31 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto and;
     case 0x2d:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto and;
     case 0x3d:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto and;
     case 0x39:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto and;
     case 0x25:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto and;
     case 0x35:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto and;
     case 0x21:
       consumed += indexed_indirect_addr(&addr, memory, mpos);
+      cycles = 6;
       goto and;
     case 0x31:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 5;
       and:
         cmd->type = AND_ASM;
         cmd->handler = bitwise_and;
@@ -82,15 +97,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto asl;
     case 0x0e:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 6;
       goto asl;
     case 0x1e:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 7;
       goto asl;
     case 0x06:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 5;
       goto asl;
     case 0x16:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 6;
       asl:
         cmd->type = ASL_ASM;
         cmd->handler = bitwise_shift_l;
@@ -116,9 +135,11 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // BIT
     case 0x2c:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto bit;
     case 0x24:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       bit:
         cmd->type = BIT_ASM;
         cmd->handler = bitwise_bit_test;
@@ -146,6 +167,7 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       cmd->type = BRK_ASM;
       addr.type = IMP_ADDR;
       cmd->handler = break_interrupt;
+      cycles = 7;
       break;
     // BVC
     case 0x50:
@@ -184,24 +206,31 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto cmp;
     case 0xcd:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto cmp;
     case 0xdd:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto cmp;
     case 0xd9:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto cmp;
     case 0xc5:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto cmp;
     case 0xd5:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto cmp;
     case 0xc1:
       consumed += indexed_indirect_addr(&addr, memory, mpos);
+      cycles = 6;
       goto cmp;
     case 0xd1:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 5;
       cmp:
         cmd->type = CMP_ASM;
         cmd->handler = compare_accumulator;
@@ -212,9 +241,11 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto cpx;
     case 0xec:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto cpx;
     case 0xe4:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       cpx:
         cmd->type = CPX_ASM;
         cmd->handler = compare_xreg;
@@ -225,9 +256,11 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto cpy;
     case 0xcc:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto cpy;
     case 0xc4:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       cpy:
         cmd->type = CPY_ASM;
         cmd->handler = compare_yreg;
@@ -235,15 +268,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // DEC
     case 0xce:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 6;
       goto dec;
     case 0xde:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 7;
       goto dec;
     case 0xc6:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 5;
       goto dec;
     case 0xd6:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 6;
       dec:
         cmd->type = DEC_ASM;
         cmd->handler = decrement_memory;
@@ -266,24 +303,31 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto eor;
     case 0x4d:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto eor;
     case 0x5d:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto eor;
     case 0x59:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto eor;
     case 0x45:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto eor;
     case 0x55:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto eor;
     case 0x41:
       consumed += indexed_indirect_addr(&addr, memory, mpos);
+      cycles = 6;
       goto eor;
     case 0x51:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 5;
       eor:
         cmd->type = EOR_ASM;
         cmd->handler = bitwise_xor;
@@ -291,15 +335,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // INC
     case 0xee:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 6;
       goto inc;
     case 0xfe:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 7;
       goto inc;
     case 0xe6:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 5;
       goto inc;
     case 0xf6:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 6;
       inc:
         cmd->type = INC_ASM;
         cmd->handler = increment_memory;
@@ -319,9 +367,11 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // JMP
     case 0x4c:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 3;
       goto jmp;
     case 0x6c:
       consumed += indirect_addr(&addr, memory, mpos);
+      cycles = 5;
       jmp:
         cmd->type = JMP_ASM;
         cmd->handler = jump;
@@ -331,6 +381,7 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       consumed += absolute_addr(&addr, memory, mpos);
       cmd->type = JSR_ASM;
       cmd->handler = jump_subroutine;
+      cycles = 6;
       break;
     // LDA
     case 0xa9:
@@ -338,24 +389,31 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto lda;
     case 0xad:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto lda;
     case 0xbd:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto lda;
     case 0xb9:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto lda;
     case 0xa5:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto lda;
     case 0xb5:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto lda;
     case 0xa1:
       consumed += indexed_indirect_addr(&addr, memory, mpos);
+      cycles = 6;
       goto lda;
     case 0xb1:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 5;
       lda:
         cmd->type = LDA_ASM;
         cmd->handler = load_accumulator;
@@ -366,15 +424,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto ldx;
     case 0xae:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ldx;
     case 0xbe:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ldx;
     case 0xa6:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto ldx;
     case 0xb6:
       consumed += zeropage_y_addr(&addr, memory, mpos);
+      cycles = 4;
       ldx:
         cmd->type = LDX_ASM;
         cmd->handler = load_xreg;
@@ -385,15 +447,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto ldy;
     case 0xac:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ldy;
     case 0xbc:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ldy;
     case 0xa4:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto ldy;
     case 0xb4:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       ldy:
         cmd->type = LDY_ASM;
         cmd->handler = load_yreg;
@@ -404,15 +470,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto lsr;
     case 0x4e:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 6;
       goto lsr;
     case 0x5e:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 7;
       goto lsr;
     case 0x46:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 5;
       goto lsr;
     case 0x56:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 6;
       lsr:
         cmd->type = LSR_ASM;
         cmd->handler = bitwise_shift_r;
@@ -426,27 +496,35 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // ORA
     case 0x09:
       consumed += immediate_addr(&addr, memory, mpos);
+
       goto ora;
     case 0x0d:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ora;
     case 0x1d:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ora;
     case 0x19:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ora;
     case 0x05:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto ora;
     case 0x15:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto ora;
     case 0x01:
       consumed += indexed_indirect_addr(&addr, memory ,mpos);
+      cycles = 6;
       goto ora;
     case 0x11:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 5;
       ora:
         cmd->type = ORA_ASM;
         cmd->handler = bitwise_or;
@@ -456,24 +534,28 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       cmd->type = PHA_ASM;
       addr.type = IMP_ADDR;
       cmd->handler = push_accumulator;
+      cycles = 3;
       break;
     // PHP
     case 0x08:
       cmd->type = PHP_ASM;
       addr.type = IMP_ADDR;
       cmd->handler = push_cpu_status;
+      cycles = 3;
       break;
     // PLA
     case 0x68:
       cmd->type = PLA_ASM;
       addr.type = IMP_ADDR;
       cmd->handler = pull_accumulator;
+      cycles = 4;
       break;
     // PLP
     case 0x28:
       cmd->type = PLP_ASM;
       addr.type = IMP_ADDR;
       cmd->handler = pull_cpu_status;
+      cycles = 4;
       break;
     // ROL
     case 0x2a:
@@ -481,15 +563,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto rol;
     case 0x2e:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 6;
       goto rol;
     case 0x3e:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 7;
       goto rol;
     case 0x26:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 5;
       goto rol;
     case 0x36:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 6;
       rol:
         cmd->type = ROL_ASM;
         cmd->handler = bitwise_rotate_l;
@@ -500,15 +586,19 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto ror;
     case 0x7e:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 6;
       goto ror;
     case 0x6e:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 7;
       goto ror;
     case 0x66:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 5;
       goto ror;
     case 0x76:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 6;
       ror:
         cmd->type = ROR_ASM;
         cmd->handler = bitwise_rotate_r;
@@ -518,12 +608,14 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       cmd->type = RTI_ASM;
       addr.type = IMP_ADDR;
       cmd->handler = return_interrupt;
+      cycles = 6;
       break;
     // RTS
     case 0x60:
       cmd->type = RTS_ASM;
       addr.type = IMP_ADDR;
       cmd->handler = return_subroutine;
+      cycles = 6;
       break;
     // SBC
     case 0xe9:
@@ -531,24 +623,31 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
       goto sbc;
     case 0xed:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto sbc;
     case 0xfd:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto sbc;
     case 0xf9:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 4;
       goto sbc;
     case 0xe5:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto sbc;
     case 0xf5:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto sbc;
     case 0xe1:
       consumed += indexed_indirect_addr(&addr, memory, mpos);
+      cycles = 6;
       goto sbc;
     case 0xf1:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 5;
       sbc:
         cmd->type = SBC_ASM;
         cmd->handler = subtract_with_carry;
@@ -569,24 +668,31 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // STA
     case 0x8d:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto sta;
     case 0x9d:
       consumed += absolute_x_addr(&addr, memory, mpos);
+      cycles = 5;
       goto sta;
     case 0x99:
       consumed += absolute_y_addr(&addr, memory, mpos);
+      cycles = 5;
       goto sta;
     case 0x85:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto sta;
     case 0x95:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       goto sta;
     case 0x81:
       consumed += indexed_indirect_addr(&addr, memory, mpos);
+      cycles = 6;
       goto sta;
     case 0x91:
       consumed += indirect_indexed_addr(&addr, memory, mpos);
+      cycles = 6;
       sta:
         cmd->type = STA_ASM;
         cmd->handler = store_accumulator;
@@ -594,12 +700,15 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // STX
     case 0x8e:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto stx;
     case 0x86:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto stx;
     case 0x96:
       consumed += zeropage_y_addr(&addr, memory, mpos);
+      cycles = 4;
       stx:
         cmd->type = STX_ASM;
         cmd->handler = store_xreg;
@@ -607,12 +716,15 @@ int parse_asm(asm6502 *cmd, memory6502 *memory, uint16_t pos) {
     // STY
     case 0x8c:
       consumed += absolute_addr(&addr, memory, mpos);
+      cycles = 4;
       goto sty;
     case 0x84:
       consumed += zeropage_addr(&addr, memory, mpos);
+      cycles = 3;
       goto sty;
     case 0x94:
       consumed += zeropage_x_addr(&addr, memory, mpos);
+      cycles = 4;
       sty:
         cmd->type = STY_ASM;
         cmd->handler = store_yreg;
