012bd5b52bec4ed0a136a8a4e861f6a86efd4233
Kacper Raczy <gfw.kra@gmail.com>
2019-11-07 19:18:51 +0100

    Moved utility programs to tools/ dir

--- Emulator/test/playground.c
+++ Emulator/test/playground.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*
  Testing bitfield portability
@@ -13,6 +14,18 @@
 */
 
 char* bin(uint8_t byte, int size) {
+  char* str = malloc((size + 1) * sizeof(char));
+  for (int i = 0; i < size; i++) {
+    if ((byte & (0x01 << (size - 1 - i))) != 0)
+      str[i] = '1';
+    else
+      str[i] = '0';
+  }
+  str[size] = '\0';
+  return str;
+}
+
+char* bin16(uint16_t byte, int size) {
   char* str = malloc((size + 1) * sizeof(char));
   for (int i = 0; i < size; i++) {
     if ((byte & (0x01 << (size - 1 - i))) != 0)
@@ -32,25 +45,86 @@ struct bit_fields {
   uint8_t bit5 : 1;
 };
 
+union bits {
+  struct {
+    uint8_t a : 5;
+    uint8_t b : 2;
+    uint8_t c : 1;
+  };
+  uint8_t byte;
+};
+
+union ppu_internal_register {
+  uint16_t address;
+  struct {
+    uint16_t x_scroll : 5;
+    uint16_t y_scroll : 5;
+    uint16_t nametable_addr : 2;
+    uint16_t _unused : 4;
+  };
+};
+
 /*
  Layout
  54333221
 */
 
 int main() {
-  struct bit_fields bf;
-  memset(&bf, 0b11110000, 1);
-  printf("Size: %lu \n", sizeof(bf));
-  uint8_t* ptr = (uint8_t *) &bf;
-  printf("bit1: %s\n", bin(bf.bit1, 1));
-  printf("bit2: %s\n", bin(bf.bits2, 2));
-  printf("bit3: %s\n", bin(bf.bits3, 3));
-  printf("bit4: %s\n", bin(bf.bit4, 1));
-  printf("bit5: %s\n", bin(bf.bit5, 1));
-  printf("Physical \n");
-  for (int i = 0; i < 8; i++) {
-    printf("Bit %d - %d \n", i, ((*ptr & (0x01 << i)) != 0));
+  union bits bb;
+  bb.byte = 0xff;
+  printf("a %s\n", bin(bb.a, 5));
+  printf("b %s\n", bin(bb.b, 2));
+  printf("c %s\n", bin(bb.c, 1));
+  printf("byte: %s\n", bin(bb.byte, 8));
+  bb.a += 1;
+  printf("a %s\n", bin(bb.a, 5));
+  printf("b %s\n", bin(bb.b, 2));
+  printf("c %s\n", bin(bb.c, 1));
+  printf("byte: %s\n", bin(bb.byte, 8));
+  bb.c += 1;
+  printf("a %s\n", bin(bb.a, 5));
+  printf("b %s\n", bin(bb.b, 2));
+  printf("c %s\n", bin(bb.c, 1));
+  printf("byte: %s\n", bin(bb.byte, 8));
+
+  clock_t t = clock();
+  for (int i = 0; i < 10000000; i++) {
+    bb.a += 1;
   }
+  t = clock() - t;
+  double time_taken = ((double)t)/CLOCKS_PER_SEC;
+  printf("Taken %lf\n", time_taken);
+
+  uint8_t val = 0;
+  t = clock();
+  for (int i = 0; i < 10000000; i++) {
+    val = (val == 31) ? 0 : val + 1;
+  }
+  t = clock() - t;
+  time_taken = ((double)t)/CLOCKS_PER_SEC;
+  printf("Taken %lf\n", time_taken); 
+
+  val = 0;
+  t = clock();
+  for (int i = 0; i < 10000000; i++) {
+    val = (val + 1) & 0x1f;
+  } 
+  t = clock() - t;
+  time_taken = ((double)t)/CLOCKS_PER_SEC;
+  printf("Taken %lf\n", time_taken);
+
+  union ppu_internal_register x;
+  x.x_scroll = 0b10101;
+  x.y_scroll = 0b10101;
+  x.nametable_addr = 0b11;
+  printf("x: %s\n", bin16(x.x_scroll, 5));
+  printf("x: %s\n", bin16(x.y_scroll, 5));
+  printf("x: %s\n", bin16(x.nametable_addr, 2));
+  printf("byte: %s\n", bin16(x.address, 16));
+
+  uint16_t value = 0x10ff;
+  uint8_t lval = 0xff;
+  printf("%04x\n", (value & 0xff00) | (uint16_t) (uint8_t) (lval + 1));
 
   return 0;
 }
