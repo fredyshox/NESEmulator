3d7c2ed23a9641030b7d2e57ea8009118b132f60
Kacper Raczy <gfw.kra@gmail.com>
2019-11-17 18:19:13 +0100

    rom-info updates

--- Disassembler/rom-info.exs
+++ Disassembler/rom-info.exs
@@ -16,7 +16,7 @@ end
 
 [path | _args] = System.argv
 <<header :: bytes-size(16), _rest::binary>> = File.read!(path)
-<<nes :: bytes-size(4), prgSize, chrSize, flags6, flags7, flags8, flags9, flags10, _padding::binary>> = header
+<<nes :: bytes-size(4), prgSize, chrSize, flags6, flags7, flags8, flags9, flags10, rest::binary>> = header
 
 if nes != <<"NES", 0x1a>> do
   IO.puts("Error: #{Path.basename(path)} is not iNES rom file.")
@@ -26,6 +26,9 @@ end
 pretty_puts.("File: ", "#{Path.basename(path)}")
 pretty_puts.("PRG ROM size: ", "#{prgSize * 16} KB")
 pretty_puts.("CHR ROM size: ", "#{chrSize * 8} KB")
+if chrSize == 0 do 
+  pretty_puts.("CHR RAM size: ", "8 KB")
+end 
 
 <<mapperLow :: little-integer-size(4),
   fourScreenMode :: size(1),
@@ -52,35 +55,37 @@ consoleTypeStr =
     _ -> "NES/Famicom"
   end
 
-pretty_puts.("Mapper number: ", "#{bor(mapperLow, mapperHigh <<< 4)}")
 pretty_puts.("Mirroring mode: ", "#{mapperMode}")
 pretty_puts.("Trainer (512-byte): ", "#{if trainer == 1, do: "present", else: "not present"}")
-pretty_puts.("Cartidge RAM: ", "#{if ram == 1, do: "present", else: "not present"}")
-pretty_puts.("iNES format: ", "#{if format == 2, do: "2.0", else: "1.0"}")
+pretty_puts.("Non-volatile memory: ", "#{if ram == 1, do: "present", else: "not present"}")
 pretty_puts.("Console type: ", "#{consoleTypeStr}")
 
-if format != 2 do
-  # iNES 1.0 stuff
-  <<prgRamSize :: little-integer-size(8)>> = <<flags8>>
-  <<_reserved :: size(7),
-    tvSystem :: size(1)>> = <<flags9>>
-  <<_ :: size(3),
-    prgRamPresence :: size(1),
-    _ :: size(2),
-    tvSystemComp :: little-integer-size(2)>> = <<flags10>>
-
-  tvSystemStr =
+uint12to15 = band(:binary.decode_unsigned(rest), 0xffffffff)
+cond do
+  format == 2 ->
+    pretty_puts.("iNES format: ", "2.0") 
+    # <<submapper :: size(4), mapperUpper2 :: size(4)>> = <<flags8>>
+  format == 0 and uint12to15 == 0 -> 
+    <<prgRamSize :: size(8)>> = <<flags8>>
+    pretty_puts.("iNES format: ", "1.0")
+    pretty_puts.("Mapper number: ", "#{bor(mapperLow, mapperHigh <<< 4)}")
+    if ram == 1 do
+      pretty_puts.("PRG RAM: ", "#{if prgRamSize == 0, do: "8 KB", else: "#{prgRamSize * 8} KB"}")
+    end
+    <<_reserved :: size(7),
+      tvSystem :: size(1)>> = <<flags9>> 
+    <<_ :: size(3),
+      _prgRamPresence :: size(1),
+      _ :: size(2),
+      tvSystemComp :: size(2)>> = <<flags10>> 
+    tvSystemStr =
     cond do
       tvSystemComp == 1 or tvSystemComp == 3 ->
         "NTSC and PAL"
       tvSystem == 1 -> "PAL"
       true -> "NTSC"
     end
-
-  pretty_puts.("TV system: ", "#{tvSystemStr}")
-  if ram == 1 do
-    pretty_puts.("PRG RAM: ", "#{if prgRamPresence == 1, do: "#{prgRamSize * 8} KB", else: "not present"}")
-  end
-else
-  # iNES 2.0 stuff
+    pretty_puts.("TV system: ", "#{tvSystemStr}") 
+  true ->
+    pretty_puts.("iNES format: ", "Archaic")
 end
